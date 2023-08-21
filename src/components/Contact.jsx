c07f043d3f1ac202d910ef2fea2e415182943224
Mohammad Tayyeb Soltaninejad <freetayyeb@gmail.com>
2023-08-21 19:59:10 +0330

    contact area modification

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,11 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 import { EarthCanvas } from "./canvas";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    console.log(e.target.value);
-  };
-
   return (
     <div className="text-white xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
@@ -28,54 +13,27 @@ const Contact = () => {
         className="flex-[0.75] bg-slate-500 p-8 rounded-2xl"
       >
         <p className="text-xl"> Get in touch</p>
-        <h3 className="text-3xl font-black">Contact.</h3>
+        <h3 className="text-3xl font-black">Social Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
+        <div className="mt-12 flex flex-col gap-8">
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className="bg-slate-600 py-4 px-6 placeholder:text-slate-400 text-white rounded-lg outline-none border-none font-medium"
-            />
+            
+            <Link
+              className="bg-slate-600  py-4 px-6  hover:text-blue-950  text-white rounded-lg outline-none border-none font-medium"
+              to="https://www.linkedin.com/in/mohamad-tayyeb-soltani/"
+              target="_blank"
+            >
+              Linkedin Profile
+            </Link>
+            <Link
+              className="bg-slate-600 my-2 py-4 px-6 hover:text-blue-950   text-white rounded-lg outline-none border-none font-medium"
+              to="https://github.com/freesultan/"
+              target="_blank"
+            >
+              Github
+            </Link>
           </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-slate-600 py-4 px-6 placeholder:text-slate-400 text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What you want to say?"
-              className="bg-slate-600 py-4 px-6 placeholder:text-slate-400 text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-
-          <button
-            type="submit"
-            className="bg-slate-600 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-slate-800"
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+        </div>
       </motion.div>
 
       <motion.div
